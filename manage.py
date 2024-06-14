e945d0f7f1511abff49e613f1a41de7b6eb4bb7e
Suri <162128598+Surisuri903@users.noreply.github.com>
2024-06-14 23:50:12 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,10 +2,14 @@
 import os
 import sys
 
-
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
-    from otree.management.cli import execute_from_command_line
-
-    execute_from_command_line(sys.argv, script_file=__file__)
+    try:
+        from otree.management.cli import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import oTree. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
